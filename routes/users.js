12f14b15cd23b3839547ab86a1a7148af51d59f1
Adisak Supatanasinkasem <adisak.55555@gmail.com>
2023-06-17 12:04:19 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ console.log(myUser);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log(myUser);
-  res.send('respond with a resource ' + myUser.name);
+  res.send('respond with a resource ' + myUser.name + " : " + myUser.telephone);
 });
 
 module.exports = router;
